5eff17d3a2aae3ffc00023d551371c108610ec24
Wiktor Dyngosz <wiktor.dyngosz@deltaunit.com>
2017-10-10 14:25:26 +0200

    firebase config move

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDi1scgc-zAK6aXzGiQF0gsVSfcTMXmO5I",
+    authDomain: "brew-other-barrels.firebaseapp.com",
+    databaseURL: "https://brew-other-barrels.firebaseio.com",
+    projectId: "brew-other-barrels",
+    storageBucket: "brew-other-barrels.appspot.com",
+    messagingSenderId: "231895779451"
+  }
+};
