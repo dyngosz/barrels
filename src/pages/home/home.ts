b17ccfa9acb5a79e5769f38091813430fe18cbac
Wiktor Dyngosz <wiktor.dyngosz@deltaunit.com>
2017-10-10 14:24:03 +0200

    Home Page testing firebase implementation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 import { NavController } from 'ionic-angular';
-
+import { AngularFireDatabase } from 'angularfire2/database';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  barrels: Observable<any[]>;
 
+  constructor(public navCtrl: NavController,
+              afDB: AngularFireDatabase) {
+    this.barrels = afDB.list('barrels').valueChanges();
   }
 
 }
