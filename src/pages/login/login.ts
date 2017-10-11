70c491b697432a7d7be2c5eb140caec9cce335fe
Wiktor Dyngosz <wiktor.dyngosz@deltaunit.com>
2017-10-11 14:01:14 +0200

    Login Page template and logic

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  LoadingController,
+  Loading,
+  AlertController
+} from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthProvider } from '../../providers/auth/auth';
 
-/**
- * Generated class for the LoginPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+
 @IonicPage()
 @Component({
   selector: 'page-login',
@@ -14,7 +18,58 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  loginForm: FormGroup;
+  loading: Loading;
+
+  constructor(public navCtrl: NavController,
+              public authData: AuthProvider,
+              public formBuilder: FormBuilder,
+              public alertCtrl: AlertController,
+              public loadingCtrl: LoadingController) {
+
+    const { required, email, minLength } = Validators;
+
+    this.loginForm = formBuilder.group({
+      email: ['', [required, email]],
+      password: ['', [required, minLength(6)]]
+    });
+  }
+
+  goToResetPassword(){
+    this.navCtrl.push('ResetPasswordPage');
+  }
+
+  createAccount(){
+    this.navCtrl.push('SignupPage');
+  }
+
+  loginUser(){
+    if (!this.loginForm.valid){
+      console.log(this.loginForm.value);
+    } else {
+      this.authData.loginUser(this.loginForm.value.email, this.loginForm.value.password)
+        .then( authData => {
+          this.navCtrl.setRoot('HomePage');
+        }, error => {
+          this.loading.dismiss().then( () => {
+            let alert = this.alertCtrl.create({
+              message: error.message,
+              buttons: [
+                {
+                  text: "Ok",
+                  role: 'cancel'
+                }
+              ]
+            });
+            alert.present();
+          });
+        });
+
+      this.loading = this.loadingCtrl.create({
+        dismissOnPageChange: true,
+      });
+      this.loading.present();
+    }
   }
 
 
